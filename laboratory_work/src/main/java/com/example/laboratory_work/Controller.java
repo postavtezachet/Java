3f78716624f06dc0378f24ce49f58369d3b9f77f
Maxim <maxim@localhost.localdomain>
2022-03-05 18:06:23 +0300

    Second try

--- laboratory_work/src/main/java/com/example/laboratory_work/Controller.java
+++ laboratory_work/src/main/java/com/example/laboratory_work/Controller.java
@@ -10,7 +10,8 @@ public class Controller {
     private static final String template = "The words \" %s \" have %d symbols \"%c\"";
     @GetMapping("/Data")
     public String get(@RequestParam(value = "words", defaultValue =
-            "World") String words, @RequestParam(value = "symbol", defaultValue =
+            "World") String words,
+                      @RequestParam(value = "symbol", defaultValue =
             "a") char symbols){
             DataClass gr = new DataClass(words,symbols);
             return String.format(template,gr.getWords(),cl.calc(gr),gr.getSymbol());
